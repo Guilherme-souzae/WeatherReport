42023fc424486c415f6317af25091dbb14c0e8fe
Guilherme-souzae <guilhermesouzae.contato@gmail.com>
2026-07-09 05:15:28 -0300

    feature: logger kafka microservice

--- server/microservices/logger/src/main/java/com/ms/logger/KafkaConsumer.java
+++ server/microservices/logger/src/main/java/com/ms/logger/KafkaConsumer.java
@@ -1,4 +1,20 @@
 package com.ms.logger;
 
+import java.util.UUID;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
 public class KafkaConsumer {
+
+    private final String instanceId = UUID.randomUUID().toString().substring(0, 8);
+
+    @KafkaListener(topics = "weather-readings")
+    public void receive(String message) {
+
+        System.out.println(
+                "[" + instanceId + "] " + message
+        );
+    }
 }
